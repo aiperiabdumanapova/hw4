e08f77b1e86186fae566845e44db54a1ce0a5f63
unknown <you@example.com>
2024-04-01 21:01:52 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function isEven(number) {
+    return number % 2 === 0;
+}
+console.log(isEven(11)); 
+console.log(isEven(12)); 
+console.log(isEven(13)); 
+console.log(isEven(14)); 
+console.log(isEven(15)); 
+console.log(isEven(16)); 
+console.log(isEven(17)); 
